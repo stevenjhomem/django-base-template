826a40d901dcd25925db9983d1c13d7c50fe0cae
stevenjhomem <sjhomem@outlook.com>
2023-02-26 13:13:20 -0600

    making fixes to the files

--- configuration_root/asgi.py
+++ configuration_root/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for 1 project.
+ASGI config for configuration_root project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configuration_root.settings')
 
 application = get_asgi_application()
