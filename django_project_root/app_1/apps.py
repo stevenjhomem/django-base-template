5f5fe8a22b0f805a7e0fb966c17cc8fb23f014ca
stevenjhomem <sjhomem@outlook.com>
2023-02-26 15:18:29 -0600

    next tier of folders and files

--- django_project_root/app_1/apps.py
+++ django_project_root/app_1/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DjangoProjectRootConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_project_root'
+    name = 'app_1'
