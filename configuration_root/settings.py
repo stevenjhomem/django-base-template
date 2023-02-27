3452b180c1f922922d99d40cbe9bcd72d3bee49d
stevenjhomem <sjhomem@outlook.com>
2023-02-26 20:21:49 -0600

    next tier of folders and files

--- configuration_root/settings.py
+++ configuration_root/settings.py
@@ -9,11 +9,18 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'django_project_root/static/'
+
+# Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'django_project_root/media/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +61,7 @@ ROOT_URLCONF = 'configuration_root.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'django_project_root/templates/', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
